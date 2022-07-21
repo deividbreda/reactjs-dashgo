b1e4f525d4e64f3ee5900115a87709bfd59da649
deividbreda <deivid.breda02@gmail.com>
2022-07-21 20:18:39 -0300

    Adicionando validações nos form e inputs com o Yup

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,24 +1,37 @@
+import { FormErrorMessage } from "@chakra-ui/react";
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from 'react-hook-form'
 
 interface InputProps extends ChakraInputProps {
     name: string,
     label?: string,
+    error?: FieldError
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, error = null, label, ...rest }, ref) => {
     return (
-        <FormControl>
-            { !!label && <FormLabel htmlFor={name}> {label} </FormLabel> }
-            <ChakraInput 
-                name={name} 
+        <FormControl isInvalid={!!error}>
+            {!!label && <FormLabel htmlFor={name}> {label} </FormLabel>}
+            <ChakraInput
+                name={name}
                 id={name}
                 focusBorderColor="pink.500"
-                bgColor="gray.900" 
-                variant="filled" 
-                _hover={{ bgColor: 'gray.900' }} 
-                size="lg" 
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{ bgColor: 'gray.900' }}
+                size="lg"
+                ref={ref}
                 {...rest}
             />
+
+            {!!error && (
+                <FormErrorMessage>
+                    {error.message}
+                </FormErrorMessage>
+            )}
         </FormControl>
     );
 }
+
+export const Input = forwardRef(InputBase);
