ebeaa478a00177c7265128c43d497bd5d2b245f9
deividbreda <deivid.breda02@gmail.com>
2022-07-21 23:10:45 -0300

    Criando um refetching nos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,7 +8,7 @@ import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from "@tanstack/react-query";
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery(['users'], async () => {
+    const { data, isLoading, isFetching, error } = useQuery(['users'], async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
@@ -25,6 +25,8 @@ export default function UserList() {
             }
         })
         return users;
+    }, {
+        staleTime: 1000 * 5,
     })
 
     const isWideVersion = useBreakpointValue({
@@ -41,7 +43,10 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal"> Usuários </Heading>
+                        <Heading size="lg" fontWeight="normal"> 
+                            Usuários 
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button
