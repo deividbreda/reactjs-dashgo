20935e4e44c050a0309e8d8615d3817f2eedc65a
deividbreda <deivid.breda02@gmail.com>
2022-07-21 22:48:03 -0300

    Puxando dados do Back-end e salvando em cache

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,19 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+        return users;
     })
 
     const isWideVersion = useBreakpointValue({
@@ -66,18 +78,22 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold"> Deivid Breda </Text>
-                                                <Text fontSize="sm" color="gray.300"> deivid.breda02@gmail.com </Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td> 04 de Abril, 2021</Td>}
-                                    </Tr>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold"> {user.name} </Text>
+                                                        <Text fontSize="sm" color="gray.300"> {user.email} </Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td> {user.createdAt} </Td>}
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
 
